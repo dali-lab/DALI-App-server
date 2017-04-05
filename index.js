d62868cc98b8f9bd4bbf326da7b54876e15c1d34
John Kotz <john.lyme@mac.com>
2017-04-04 21:07:06 -0400

    Fixing port

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ app.post('/timLocation', function (req, res) {
   res.send('Noted');
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log('DALI App Server listening on port 3000!')
 })
