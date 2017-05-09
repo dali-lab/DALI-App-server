b6f0f87f72a6312b5477319ee00c3196f8560771
John Kotz <john.lyme@mac.com>
2017-05-09 14:01:38 -0400

    Adding some loggin

--- LocationTracking.js
+++ LocationTracking.js
@@ -7,199 +7,207 @@ var SharedUser = require('./SharedUser')
 var TimLocation = require('./TimLocation')
 
 /**
- Called upon entering or exiting the DALI lab (and when some settings are changed)
+Called upon entering or exiting the DALI lab (and when some settings are changed)
 
- Sample req.params:
-   {
-    user: {
-      email: 'john.kotz@dali.dartmouth.edu',
-      id: '123456789012345678901',
-      familyName: 'Kotz',
-      givenName: 'John',
-      name: 'John Kotz'
-    },
-    inDALI: false,
-    share: false
-  }
- */
+Sample req.params:
+{
+user: {
+email: 'john.kotz@dali.dartmouth.edu',
+id: '123456789012345678901',
+familyName: 'Kotz',
+givenName: 'John',
+name: 'John Kotz'
+},
+inDALI: false,
+share: false
+}
+*/
 router.post('/enterExit', function (req, res) {
-  if (req.body.user == undefined || req.body.user.email == undefined || req.body.user.email == "") {
-    res.status(500).send("Failed, you need a user object and email: " + JSON.stringify(req.body));
-    return;
-  }
+   if (req.body.user == undefined || req.body.user.email == undefined || req.body.user.email == "") {
+      res.status(500).send("Failed, you need a user object and email: " + JSON.stringify(req.body));
+      return;
+   }
 
-  // Getting the user from the db...
-  SharedUser.find({email: req.body.user.email}).then((users) => {
-    // Make certain I have no extra users
-    users = users.filter((user) => {
-      return user.email == req.body.user.email;
-    });
+   console.log("Enter exit...", req.body);
+   // Getting the user from the db...
+   SharedUser.find({email: req.body.user.email}).then((users) => {
+      // Make certain I have no extra users
+      users = users.filter((user) => {
+         return user.email == req.body.user.email;
+      });
 
-    if (users.length > 0) {
-      // If there is a user I would like to update it
-      var user = users[0];
-      user.shared = req.body.share;
+      console.log("Matching users:", users);
 
+      if (users.length > 0) {
+         // If there is a user I would like to update it
+         var user = users[0];
+         user.shared = req.body.share;
+         console.log("Shared is now:", user.shared);
 
-      // If you are just now turning it on, I will save your location too
-      if (req.body.share) {
-        user.inDALI = req.body.inDALI;
+         // If you are just now turning it on, I will save your location too
+         if (req.body.share) {
+            user.inDALI = req.body.inDALI;
 
-        if (req.body.inDALI) {
-          setInterval(function() {
-            if (user.lastUpdate - new Date() >= 1000 * 60 * 30) {
-              // We have received no update since
-              user.inDALI = false;
-              user.save().then(() => {});
+            console.log("Starting interval...");
+            if (req.body.inDALI) {
+               setInterval(function() {
+                  console.log("Interval...");
+                  if (user.lastUpdate - new Date() >= 1000 * 60 * 30) {
+                     // We have received no update since
+                     user.inDALI = false;
+                     user.save().then(() => {});
+                     console.log("Cancelling inDALI...");
+                  }
+               }, 1000 * 60 * 30);
             }
-          }, 1000 * 60 * 30);
-        }
+         }
+
+         user.lastUpdate = new Date();
+
+         // Save...
+         console.log("Saving user...");
+         user.save().then((user) => {
+            console.log("Done");
+            res.send('Noted');
+         });
+      }else if (req.body.share){
+         // This user is new to sharing. I will create a new record with their data
+         var user = new SharedUser({
+            email: req.body.user.email,
+            name: req.body.user.name,
+            inDALI: req.body.inDALI,
+            shared: req.body.share,
+            lastUpdate: new Date()
+         });
+
+         // And save...
+         user.save().then((user) => {
+            res.send('Noted');
+         });
       }
-
-      user.lastUpdate = new Date();
-
-      // Save...
-      user.save().then((user) => {
-        res.send('Noted');
-      });
-    }else if (req.body.share){
-      // This user is new to sharing. I will create a new record with their data
-      var user = new SharedUser({
-        email: req.body.user.email,
-        name: req.body.user.name,
-        inDALI: req.body.inDALI,
-        shared: req.body.share,
-        lastUpdate: new Date()
-      });
-
-      // And save...
-      user.save().then((user) => {
-        res.send('Noted');
-      });
-    }
-  }).catch((error) => {
-    console.error("Encountered an error running enterExit!");
-    console.error(error);
-    res.send(error);
-  });
+   }).catch((error) => {
+      console.error("Encountered an error running enterExit!");
+      console.error(error);
+      res.send(error);
+   });
 });
 
 function resetLocations() {
-  return SharedUser.find().then((users) => {
-    var promises = []
+   return SharedUser.find().then((users) => {
+      var promises = []
 
-    users.forEach((user) => {
-      user.inDALI = false;
-      promises.push(user.save());
-    });
+      users.forEach((user) => {
+         user.inDALI = false;
+         promises.push(user.save());
+      });
 
-    return Promise.all(promises)
-  }).then(() => {
-    return TimLocation.find()
-  }).then((tims) => {
-    var promises = []
+      return Promise.all(promises)
+   }).then(() => {
+      return TimLocation.find()
+   }).then((tims) => {
+      var promises = []
 
-    tims.forEach((tim) => {
-      tim.inDALI = false;
-      tim.inOffice = false;
-      promises.push(tim.save());
-    });
+      tims.forEach((tim) => {
+         tim.inDALI = false;
+         tim.inOffice = false;
+         promises.push(tim.save());
+      });
 
-    return Promise.all(promises)
-  });
+      return Promise.all(promises)
+   });
 }
 
 router.post("/reset", function(req, res) {
-  resetLocations().then(() => {
-    res.send("Complete!");
-  }).catch((error) => {
-    res.send("Failed! " + error);
-  });
+   resetLocations().then(() => {
+      res.send("Complete!");
+   }).catch((error) => {
+      res.send("Failed! " + error);
+   });
 });
 
 /**
- Get a list of users in the lab who share their location
+Get a list of users in the lab who share their location
 
- RETURNS:
-  [
-    {
-      email: String,
-      name: String,
-      inDALI: Bool,
-      shared: Bool
-    }
-  ]
- */
+RETURNS:
+[
+{
+email: String,
+name: String,
+inDALI: Bool,
+shared: Bool
+}
+]
+*/
 router.get('/shared', function(req, res) {
-  SharedUser.find({shared: true, inDALI: true}).then((users) => {
-    res.json(users.filter((user) => {
-      return user.email != null
-        && user.name != null
-        && user.inDALI != null
-        && user.shared != null
-        && user.email != ""
-        && user.name != "";
-    }));
-  })
+   SharedUser.find({shared: true, inDALI: true}).then((users) => {
+      res.json(users.filter((user) => {
+         return user.email != null
+         && user.name != null
+         && user.inDALI != null
+         && user.shared != null
+         && user.email != ""
+         && user.name != "";
+      }));
+   })
 });
 
 /**
- Post Tim's current location
+Post Tim's current location
 
- PARAMETERS: {
-    location: "OFFICE",
-    enter: true
-  }
- */
+PARAMETERS: {
+location: "OFFICE",
+enter: true
+}
+*/
 router.post('/tim', function (req, res) {
-  // Get the last entry
-  TimLocation.find().then((locations) => {
-    // Create a placeholder to put the correct record into
-    var loc = null;
+   // Get the last entry
+   TimLocation.find().then((locations) => {
+      // Create a placeholder to put the correct record into
+      var loc = null;
 
-    if (locations.length > 0) {
-      // If there is one already...
-      loc = locations[0]
-    }else{
-      // If there isn't one already, create new one.
-      var loc = new TimLocation({
-        inDALI: false,
-        inOffice: false
+      if (locations.length > 0) {
+         // If there is one already...
+         loc = locations[0]
+      }else{
+         // If there isn't one already, create new one.
+         var loc = new TimLocation({
+            inDALI: false,
+            inOffice: false
+         })
+      }
+
+      // With the record, store the enter value in the relevant value
+      if (req.body.location == "DALI") {
+         loc.inDALI = req.body.enter
+      }else if (req.body.location == "OFFICE") {
+         loc.inOffice = req.body.enter
+      }
+
+      // Save...
+      loc.save().then((loc) => {
+         res.send('Noted')
       })
-    }
-
-    // With the record, store the enter value in the relevant value
-    if (req.body.location == "DALI") {
-      loc.inDALI = req.body.enter
-    }else if (req.body.location == "OFFICE") {
-      loc.inOffice = req.body.enter
-    }
-
-    // Save...
-    loc.save().then((loc) => {
-      res.send('Noted')
-    })
-  })
+   })
 });
 
 /**
- Get Tim's last known location
+Get Tim's last known location
 
- RETURNS: {
-   inDALI: Bool,
-   inOffice: Bool,
-  }
- */
+RETURNS: {
+inDALI: Bool,
+inOffice: Bool,
+}
+*/
 router.get('/tim', function(req, res) {
-  TimLocation.find().then((locations) => {
-    if (locations.length > 0) {
-      // Return the record
-      res.json(locations[0])
-    }else{
-      // No record. Report unknown
-      res.json({inDALI: false, inOffice: false})
-    }
-  })
+   TimLocation.find().then((locations) => {
+      if (locations.length > 0) {
+         // Return the record
+         res.json(locations[0])
+      }else{
+         // No record. Report unknown
+         res.json({inDALI: false, inOffice: false})
+      }
+   })
 });
 
 module.exports = router;
